be1b1e107a52dde9dafeba8e1d7bea9fa4089319
hittmeyer001 <52324314+hittmeyer001@users.noreply.github.com>
2019-07-09 16:20:07 +0300

    Add files via upload
    You can use this in your project as home page

---
+++ meru/index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>County government Of Meru</title>
+    <link rel="icon" type="image/png" href="img/download.png">
+
+    <link href='https://fonts.googleapis.com/css?family=Oxygen:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body id="home">
+
+<div class="wrapper">
+
+    <header id="header" class="">
+     
+        <div class="header-wrapper">
+            <div class="header-container">
+                <div>
+                    <div class="logo-name">
+                        <div class="logo"><a href="index.php" title=""><img src="img/download.png" width="150px" height="150x" 
+                         alt="logo"></a></div>
+                                                                             
+                        <div class="name">
+                            <a href="index.php" title="">
+                             <h3>WELCOME TO COUNTY GOVERNMENT OF MERU</h3>
+                             <div class="text-center">
+                                <h4 class="primary-color">TUFANYE KAZI</h4>
+                            </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <nav>
+            <div class="navbar navbar-inverse navbar-custom" role="navigation">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse"
+                                data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            <li>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Meru <b
+                                        class="caret"></b></a>
+                                <ul class="dropdown-menu multi-level">
+                                    <li><a href="#">About Meru</a></li>
+                                    <li><a href="#">Meruu</a></li>
+
+                                    
+                                    <li class="dropdown-submenu">
+                                       
+                                        <ul class="dropdown-menu">
+                                            
+                                            
+
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Administration<b
+                                        class="caret"></b></a>
+                                <ul class="dropdown-menu multi-level">
+                                    <li class="dropdown-submenu">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Executive</a>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="#">Imamu</a></li>
+                                             <li><a href="#">Secretary</a></li>
+                                            <li><a href="#">Treasurer</a></li>
+                                        </ul>
+
+
+                                    </li>
+                                     <li><a href="#">Other Members</a></li>
+                                    <li class="dropdown-submenu">
+                                        
+                                        <ul class="dropdown-menu">
+                                           
+                                        </ul>
+                                    </li>
+                                    
+                                    
+
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Events <b
+                                        class="caret"></b></a>
+                                <ul class="dropdown-menu multi-level">
+                                    <li><a href="#">Prize Awarrd</a></li>
+                                    <li><a href="#">Prize</a></li>
+                                    <li><a href="#">prize</a></li>
+                                    
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Partners <b class="caret"></b></a>
+                                <ul class="dropdown-menu multi-level">
+                                    <li><a href="#">Popular partners</a></li>
+                                   
+                                
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Big Project <b
+                                        class="caret"></b></a>
+                                <ul class="dropdown-menu multi-level">
+                                   
+                                     <li><a href="#">Project Details</a></li>
+                                   
+                                    
+                                    
+                                </ul>
+                            </li>
+
+                            <li><a href="#">Contacts</a></li>
+                              <li><a href="#">Donations</a></li>
+                            <li><a href="#">Photo Gallery</a></li>
+                        </ul>
+                    </div>
+               
+                </div>
+            </div>
+        </nav>
+    </header>
+  
+
+    <div class="carousel-wrapper">
+        <div class="carousel" style="margin: 0 auto">
+            <div class="carousel-items">
+                <img height="400px" src="m/meru1.jpg" alt="meru">
+                <div></div>
+            </div>
+            <div class="carousel-items">
+              <img height="400px" src="m/meru1.jpg" alt="meru">
+                <div></div>
+            </div>
+            <div class="carousel-items">
+               <img height="400px" src="m/meru1.jpg" alt="meru">
+                <div></div>
+                </div>
+            <div class="carousel-items">
+                <img height="400px" src="m/meru1.jpg" alt="meru">
+                <div></div>
+            </div>
+            <div class="carousel-items">
+                <img height="400px" src="m/meru1.jpg" alt="meru">
+                <div></div>
+            </div>
+            
+            <div class="carousel-items">
+ <img height="400px" src="m/meru1.jpg" alt="meru">
+                <div></div>
+            </div>
+        </div>
+    </div>
+
+    
+    <div class="widgets-wrapper">
+        <div class="widgets-container">
+
+            
+            <div class="alert alert-warning text-center">
+                 <a class="alert-link" href="#">County governmemnt of Meru.
+               </a>
+            </div>
+
+        
+            
+
+                       
+        </div>
+    </div>
+      
+
+
+
+
+    <div class="container">
+        <div class="row">
+<div class="col-lg-3">
+<h2>Mission</h2>
+<P>We have launched The Meru county Talent Academy at Meru Teachers College. It is a dream come true for us as one of my pledges in the campaigns. was to focus on unleashing the potential of our young people. Investing in the youth is in investing in prosperity, stability and cohesion in our county
+    </P>
+
+</div>
+
+<div class="col-lg-3">
+<h2>Vision</h2>
+<p>We have launched The Meru county Talent Academy at Meru Teachers College. It is a dream come true for us as one of my pledges in the campaigns, was to focus on unleashing the potential of our young people. Investing in the youth is in investing in prosperity, stability and cohesion in our county
+
+    </p>
+
+</div>
+
+
+
+
+<div class="col-lg-6">
+
+<h2>Background Information</h2>
+<p>We have launched The Meru county Talent Academy at Meru Teachers College. It is a dream come true for us as one of my pledges in the campaigns, was to focus on unleashing the potential of our young people. Investing in the youth is in investing in prosperity, stability and cohesion in our county
+
+    </p>
+
+</div>
+</div>
+</div>
+ 
+    <footer>
+        
+    </footer>
+
+</div>
+<script   src="https://code.jquery.com/jquery-2.2.4.min.js"   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="   crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/jquery.paginate.min.js "></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<script src="https://use.fontawesome.com/434d359724.js"></script>
+<script src="js/main.js"></script>
+<script>
+    $(document).ready(function () {
+        $('[data-toggle="popover"]').popover();
+        $('[data-toggle="popover"]').on('click', function (e) {
+            $('[data-toggle="popover"]').not(this).popover('hide');
+        });
+    });
+</script>
+
+
+
+</body>
+
+</html>
